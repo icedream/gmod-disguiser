95d24ee304a7f1bfa5179d9b5e97c5f0b4b27e03
icedream <icedream2k9@die-optimisten.net>
2013-12-21 00:02:40 +0100

    LocalPlayer():GetWeapon == nil, wat!?

--- lua/weapons/disguiser/cl_fxfake.lua
+++ lua/weapons/disguiser/cl_fxfake.lua
@@ -33,9 +33,9 @@ usermessage.Hook("disguiserShootFX", function(um)
 	local bFirstTimePredicted = um:ReadBool()
 	
 	// Player and weapon valid?
-	if !IsValid(LocalPlayer()) || !IsValid(LocalPlayer():GetWeapon("disguiser")) then return false end
+	if !IsValid(LocalPlayer()) || !IsValid(LocalPlayer():GetWeapon()) || !IsValid(LocalPlayer():GetWeapon().DoShootEffect then return false end
 	
 	// Render shoot effect
-	LocalPlayer():GetWeapon("disguiser").DoShootEffect(
+	LocalPlayer():GetWeapon("disguiser"):DoShootEffect(
 		hitpos, hitnormal, entity, physbone, bFirstTimePredicted)
 end)
