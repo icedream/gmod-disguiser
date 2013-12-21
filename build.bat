a3492505ace356ea981ee259829914afa2fbc0ac
icedream <icedream2k9@die-optimisten.net>
2013-12-21 21:14:42 +0100

    Create builds/ folder if not existing.

--- build.bat
+++ build.bat
@@ -1,5 +1,6 @@
 @echo off
 path %path%;%programfiles(x86)%\Steam\SteamApps\common\GarrysMod\bin
+if not exist builds mkdir builds
 mkdir tmp
 robocopy . tmp *.json *.lua *.wav *.mp3 *.jpg *.png *.txt /E /XD tmp /XF LICENSE.txt
 gmad create -folder "tmp" -out "builds\disguiser_swep.gma"
