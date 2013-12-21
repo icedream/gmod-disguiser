3a64efe6c9c0bd1dc79c28290306c920b1da9263
icedream <icedream2k9@die-optimisten.net>
2013-12-21 21:13:37 +0100

    Build script.

---
+++ build.bat
@@ -0,0 +1,7 @@
+@echo off
+path %path%;%programfiles(x86)%\Steam\SteamApps\common\GarrysMod\bin
+mkdir tmp
+robocopy . tmp *.json *.lua *.wav *.mp3 *.jpg *.png *.txt /E /XD tmp /XF LICENSE.txt
+gmad create -folder "tmp" -out "builds\disguiser_swep.gma"
+rmdir /q /s tmp
+pause
